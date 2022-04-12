17ffc9e7a1220ee81dfe3165e88a76ae32d09690
ranggatw <rangga.public@gmail.com>
2022-04-13 01:34:08 +0700

    setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import Logo from "./assets/image.png";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>Hello</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="wrapper">
+        <h1>Bank Milenial</h1>
+        <img src={Logo} />
+        <h1>
+          coming soon<span className="dot">.</span>
+        </h1>
+        <hr />
+        <p>on April 2022</p>
+      </div>
     </div>
   );
 }
---
+++ src/pages/01_WellcomePage/WellcomePage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function WellcomePage() {
+  return (
+    <div>
+      <div>Hello</div>
+    </div>
+  );
+}
+
+export default WellcomePage;
